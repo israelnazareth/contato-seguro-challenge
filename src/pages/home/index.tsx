9e0fbcab4ef1d61e1ddce7d42bb27b036d269066
Israel Nazareth <israelsnazareth@gmail.com>
2023-04-06 19:50:11 -0300

    feat: starts modal with react-modal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,29 +1,91 @@
 import Image from "next/image"
 import logo from '../../assets/logo.png';
-import { PencilSimpleLine, Plus, Trash } from "@phosphor-icons/react";
-import { Container, Fields, Main, Table } from "./styles"
+import data from '../../data/data.json';
+import { PencilSimpleLine, Plus, Trash, X } from "@phosphor-icons/react";
+import { Container, Fields, ModalHeader, Main, Table, customModal, ModalForm, ModalFormButtons, PhoneAndBirthday } from "./styles"
 import { useState } from "react";
+import Modal from 'react-modal';
+
+interface ObjData {
+  name: string,
+  email: string,
+  phone: string,
+  birthday: string,
+  city: string
+}
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedOption, setSelectedOption] = useState('name');
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const filteredObj = inputValue.length > 0 ?
+    data.filter(item => (
+      item[selectedOption as keyof ObjData].toLowerCase()
+        .includes(inputValue.toLowerCase()))
+    ) : data
+
   return (
     <>
       <Main>
         <Container>
           <Image src={logo} alt='' />
           <Fields>
-            <button>
+            <button onClick={() => setIsOpen(true)}>
               <Plus color="#FFF" size={26} weight="bold" />
             </button>
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={() => setIsOpen(false)}
+              style={customModal}
+            >
+              <ModalHeader>
+                <h2>Inserir</h2>
+                <button onClick={() => setIsOpen(false)}>
+                  <X color="#EEE" size={30} weight="bold" />
+                </button>
+              </ModalHeader>
+              <ModalForm>
+                <label htmlFor="name">
+                  <span>Nome:</span>
+                  <input id="name" type="text" min={3} />
+                </label>
+                <label htmlFor="email">
+                  <span>E-mail:</span>
+                  <input id="email" type="email" />
+                </label>
+                <PhoneAndBirthday>
+                  <label htmlFor="phone">
+                    <span>Telefone:</span>
+                    <input id="phone" type="tel" />
+                  </label>
+                  <label htmlFor="birthday">
+                    <span>Data de Nascimento:</span>
+                    <input id="birthday" type="date" />
+                  </label>
+                </PhoneAndBirthday>
+                <label htmlFor="city">
+                  <span>Cidade onde nasceu:</span>
+                  <input id="city" type="text" />
+                </label>
+                <ModalFormButtons>
+                  <button onClick={(e) => e.preventDefault()}>Limpar</button>
+                  <button onClick={(e) => e.preventDefault()}>Enviar</button>
+                </ModalFormButtons>
+              </ModalForm>
+            </Modal>
             <input
               type="search"
               placeholder="Buscar..."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
             />
-            <select>
-              <option>Nome</option>
-              <option>Email</option>
-              <option>Telefone</option>
-              <option>Nascimento</option>
-              <option>Cidade</option>
+            <select onChange={(e) => setSelectedOption(e.target.value)}>
+              <option value="name">Nome</option>
+              <option value="email">Email</option>
+              <option value="phone">Telefone</option>
+              <option value="birthday">Nascimento</option>
+              <option value="city">Cidade</option>
             </select>
           </Fields>
           <Table>
@@ -37,48 +99,14 @@ export default function Home() {
               </tr>
             </thead>
             <tbody>
-              {Array.from({ length: 3 }).map(() => (
+              {filteredObj.map(value => (
                 <>
                   <tr>
-                    <td>Fulano</td>
-                    <td>fulano@tal.com.br</td>
-                    <td>(12) 3456-7890</td>
-                    <td>01/01/1990</td>
-                    <td>Rio de Janeiro</td>
-                    <td>
-                      <button>
-                        <PencilSimpleLine size={32} color="#FFF" />
-                      </button>
-                    </td>
-                    <td>
-                      <button>
-                        <Trash size={32} color="#FFF" />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Beltrano</td>
-                    <td>beltrano@tal.com.br</td>
-                    <td>(12) 3456-7890</td>
-                    <td>01/01/1990</td>
-                    <td>Rio de Janeiro</td>
-                    <td>
-                      <button>
-                        <PencilSimpleLine size={32} color="#FFF" />
-                      </button>
-                    </td>
-                    <td>
-                      <button>
-                        <Trash size={32} color="#FFF" />
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>Cicrano</td>
-                    <td>cicrano@tal.com.br</td>
-                    <td>(12) 3456-7890</td>
-                    <td>01/01/1990</td>
-                    <td>Rio de Janeiro</td>
+                    <td>{value.name}</td>
+                    <td>{value.email}</td>
+                    <td>{value.phone}</td>
+                    <td>{value.birthday}</td>
+                    <td>{value.city}</td>
                     <td>
                       <button>
                         <PencilSimpleLine size={32} color="#FFF" />
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -26,6 +26,8 @@ export const Container = styled.div`
 
 export const Fields = styled.div`
   display: flex;
+  justify-content: space-between;
+  width: 100%;
   gap: 1.5rem;
   margin-top: 1rem;
 
@@ -69,5 +71,83 @@ export const Table = styled.table`
       background-color: #000;
       padding: 2px;
     }
+  }
+`;
+
+export const customModal = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '35rem'
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  }
+};
+
+export const ModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #000;
+    border: none;
+    border-radius: 4px;
+    padding: 1px;
+    cursor: pointer;
+  }
+`;
+
+export const ModalForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    justify-content: center;
+  }
+
+  input {
+    width: 100%;
+    padding: 0.5rem;
+    border-radius: 4px;
+    border: 1px solid #aaa;
+  }
+`;
+
+export const PhoneAndBirthday = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  
+  label {
+    width: 100%;
+  }
+`;
+
+export const ModalFormButtons = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  button {
+    padding: 0.5rem 0.8rem;
+    border: none;
+    background-color: #000;
+    color: #eee;
+    border-radius: 4px;
+    cursor: pointer;
   }
 `;
