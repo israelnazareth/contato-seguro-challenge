76fb5ece79686c4428dd5d128ca14a1978184ed6
Israel Nazareth <israelsnazareth@gmail.com>
2023-04-06 00:53:01 -0300

    feat: starts table with mocked data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,71 @@
+import Image from "next/image"
+import logo from '../../assets/logo.png';
+import { Plus } from "@phosphor-icons/react";
+import { Container, Fields, Main, Table } from "./styles"
+import { useState } from "react";
+
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Main>
+        <Container>
+          <Image src={logo} alt='' />
+          <Fields>
+            <button>
+              <Plus color="#FFF" size={26} weight="bold" />
+            </button>
+            <input
+              type="search"
+              placeholder="Buscar..."
+            />
+            <select>
+              <option>Nome</option>
+              <option>Email</option>
+              <option>Telefone</option>
+              <option>Nascimento</option>
+              <option>Cidade</option>
+            </select>
+          </Fields>
+          <Table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Telefone</th>
+                <th>Nascimento</th>
+                <th>Cidade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.from({ length: 3 }).map(() => (
+                <>
+                  <tr>
+                    <td>Fulano</td>
+                    <td>fulano@tal.com.br</td>
+                    <td>(12) 3456-7890</td>
+                    <td>01/01/1990</td>
+                    <td>Rio de Janeiro</td>
+                  </tr>
+                  <tr>
+                    <td>Beltrano</td>
+                    <td>beltrano@tal.com.br</td>
+                    <td>(12) 3456-7890</td>
+                    <td>01/01/1990</td>
+                    <td>Rio de Janeiro</td>
+                  </tr>
+                  <tr>
+                    <td>Cicrano</td>
+                    <td>cicrano@tal.com.br</td>
+                    <td>(12) 3456-7890</td>
+                    <td>01/01/1990</td>
+                    <td>Rio de Janeiro</td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+      </Main >
     </>
   )
 }
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  height: 100%;
+  width: 100vw;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  background-image: url(https://images.unsplash.com/photo-1582653547187-2bb73b2d7b67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80);
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #EEE;
+  padding: 1rem;
+  border-radius: 8px;
+`;
+
+export const Fields = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  margin-top: 1rem;
+
+  > button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 2.5rem;
+    height: 2.5rem;
+    background-color: #000;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  > input {
+    width: 25rem;
+    padding: 0 0.5rem;
+    border: 1px solid gray;
+    border-radius: 4px;
+  }
+`
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  margin-top: 1rem;
+
+  th, td {
+    border-top: 1px solid #DDD;
+    text-align: left;
+    padding: 0.75rem;
+  }
+`;
