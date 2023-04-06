61e7190dbfd1de18fec51b4ab055742c32f2af29
Israel Nazareth <israelsnazareth@gmail.com>
2023-04-05 22:45:33 -0300

    feat: first commit with styled components

---
+++ src/styles/globals.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  body, html {
+    height: 100%;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, button, input, textarea {
+    font: 400 1rem sans-serif;
+  }
+`;
