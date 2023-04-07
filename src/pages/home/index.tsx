a59b63a68f263d7e655b6d9c8a16e93456bcb69e
Israel Nazareth <israelsnazareth@gmail.com>
2023-04-07 00:13:30 -0300

    feat: starts the edit button logic

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,10 +3,10 @@ import logo from '../../assets/logo.png';
 import data from '../../data/data.json';
 import { PencilSimpleLine, Plus, Trash, X } from "@phosphor-icons/react";
 import { Container, Fields, ModalHeader, Main, Table, customModal, ModalForm, ModalFormButtons, PhoneAndBirthday } from "./styles"
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from 'react-modal';
 
-interface ObjData {
+interface ObjDataTypes {
   name: string,
   email: string,
   phone: string,
@@ -14,16 +14,59 @@ interface ObjData {
   city: string
 }
 
+const initialStateObj: ObjDataTypes = {
+  name: '',
+  email: '',
+  birthday: '',
+  city: '',
+  phone: ''
+}
+
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
-  const [selectedOption, setSelectedOption] = useState('name');
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('name');
+  const [contactObject, setContactObject] = useState(initialStateObj);
+  const [contactPosition, setContactPosition] = useState(0);
 
   const filteredObj = inputValue.length > 0 ?
     data.filter(item => (
-      item[selectedOption as keyof ObjData].toLowerCase()
+      item[selectedOption as keyof ObjDataTypes].toLowerCase()
         .includes(inputValue.toLowerCase()))
     ) : data
+
+  function handleInsertNewContact() {
+    setIsOpen(true);
+    setContactObject(initialStateObj)
+  }
+
+  function handleEditButton(obj: ObjDataTypes, index: number) {
+    setIsOpen(true);
+    setContactObject(obj);
+    setContactPosition(index);
+  }
+
+  function handleEditInput(e: any) {
+    const { name, value } = e.target;
+    setContactObject({ ...contactObject, [name]: value })
+  }
+
+  function handleClearFields(e: any) {
+    e.preventDefault();
+    setContactObject(initialStateObj)
+  }
+
+  function handleSendConfirm(e: any) {
+    e.preventDefault();
+    filteredObj[contactPosition] = contactObject;
+    setIsOpen(false);
+  }
+
+  function handleRemoveContact(index: number) {
+    if (confirm("Tem certeza que deseja excluir este contato?")) {
+      filteredObj.splice(index, 1);
+    }
+  }
 
   return (
     <>
@@ -31,13 +74,14 @@ export default function Home() {
         <Container>
           <Image src={logo} alt='' />
           <Fields>
-            <button onClick={() => setIsOpen(true)}>
+            <button onClick={handleInsertNewContact}>
               <Plus color="#FFF" size={26} weight="bold" />
             </button>
             <Modal
               isOpen={modalIsOpen}
               onRequestClose={() => setIsOpen(false)}
               style={customModal}
+              ariaHideApp={false}
             >
               <ModalHeader>
                 <h2>Inserir</h2>
@@ -48,29 +92,60 @@ export default function Home() {
               <ModalForm>
                 <label htmlFor="name">
                   <span>Nome:</span>
-                  <input id="name" type="text" min={3} />
+                  <input
+                    id="name"
+                    type="text"
+                    min={3}
+                    name="name"
+                    value={contactObject.name}
+                    onChange={handleEditInput}
+                  />
                 </label>
                 <label htmlFor="email">
                   <span>E-mail:</span>
-                  <input id="email" type="email" />
+                  <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={contactObject.email}
+                    onChange={handleEditInput}
+                  />
                 </label>
                 <PhoneAndBirthday>
                   <label htmlFor="phone">
                     <span>Telefone:</span>
-                    <input id="phone" type="tel" />
+                    <input
+                      id="phone"
+                      type="tel"
+                      name="phone"
+                      value={contactObject.phone}
+                      onChange={handleEditInput}
+                    />
                   </label>
                   <label htmlFor="birthday">
                     <span>Data de Nascimento:</span>
-                    <input id="birthday" type="date" />
+                    <input
+                      id="birthday"
+                      type="text"
+                      name="birthday"
+                      value={contactObject.birthday}
+                      onChange={handleEditInput}
+                    />
                   </label>
                 </PhoneAndBirthday>
                 <label htmlFor="city">
                   <span>Cidade onde nasceu:</span>
-                  <input id="city" type="text" />
+                  <input
+                    id="city"
+                    type="text"
+                    name="city"
+                    value={contactObject.city}
+                    onChange={handleEditInput}
+                  />
                 </label>
                 <ModalFormButtons>
-                  <button onClick={(e) => e.preventDefault()}>Limpar</button>
-                  <button onClick={(e) => e.preventDefault()}>Enviar</button>
+                  <button onClick={handleClearFields}>Limpar</button>
+                  <button onClick={handleSendConfirm}>Confirmar</button>
                 </ModalFormButtons>
               </ModalForm>
             </Modal>
@@ -99,26 +174,24 @@ export default function Home() {
               </tr>
             </thead>
             <tbody>
-              {filteredObj.map(value => (
-                <>
-                  <tr>
-                    <td>{value.name}</td>
-                    <td>{value.email}</td>
-                    <td>{value.phone}</td>
-                    <td>{value.birthday}</td>
-                    <td>{value.city}</td>
-                    <td>
-                      <button>
-                        <PencilSimpleLine size={32} color="#FFF" />
-                      </button>
-                    </td>
-                    <td>
-                      <button>
-                        <Trash size={32} color="#FFF" />
-                      </button>
-                    </td>
-                  </tr>
-                </>
+              {filteredObj.map((obj, i) => (
+                <tr key={i}>
+                  <td>{obj.name}</td>
+                  <td>{obj.email}</td>
+                  <td>{obj.phone}</td>
+                  <td>{obj.birthday}</td>
+                  <td>{obj.city}</td>
+                  <td>
+                    <button onClick={() => handleEditButton(obj, i)}>
+                      <PencilSimpleLine size={32} color="#FFF" />
+                    </button>
+                  </td>
+                  <td>
+                    <button onClick={() => handleRemoveContact(i)}>
+                      <Trash size={32} color="#FFF" />
+                    </button>
+                  </td>
+                </tr>
               ))}
             </tbody>
           </Table>
